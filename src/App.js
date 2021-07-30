45bb9a90018d1a4adc6bfb1aadae37c45b45f7cf
João Vitor Zeppe <67124022+Calebzaw@users.noreply.github.com>
2021-07-30 20:18:03 -0300

    projeto começado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import react from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+const Site = styled.div`
+    font-size: 20px;
+    padding: 10px 15px;
+    border: 3px solid #000;
+    background-color: purple;
+    color: #CCC;
+`;
 
+function App(){
 
+    return(
+        <>
+            <Site>
+                Olá Mundo!
+            </Site>
+        </>
+    );
+}
 
-
+export default App;
 
