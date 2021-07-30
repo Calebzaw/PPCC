ed30e6db498ca922ab6956c65753609995c03e26
João Vitor Zeppe <67124022+Calebzaw@users.noreply.github.com>
2021-07-30 20:34:59 -0300

    limpeza

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+// Checar https://github.com/Hermanya/awesome-react-bootstrap-components para conponentes legais
+
 const Site = styled.div`
     font-size: 20px;
     padding: 10px 15px;
     border: 3px solid #000;
     background-color: purple;
     color: #CCC;
+    width: 100px;
+    height: 100px;
 `;
 
 function App(){
 
+    const alertTopi = () => {
+        alert('Topi');
+    }
+
     return(
         <>
             <Site>
-                Olá Mundo!
+                <button onClick={alertTopi}>Clique aqui</button>
             </Site>
         </>
     );
