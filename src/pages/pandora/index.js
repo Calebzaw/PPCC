94006b51b3d7e552f395d8f7507a2a7ab1d918df
João Vitor Zeppe <67124022+Calebzaw@users.noreply.github.com>
2021-09-27 20:47:27 -0300

    align ecologia

--- src/pages/pandora/index.js
+++ src/pages/pandora/index.js
@@ -34,6 +34,7 @@ const TextArea = styled.div`
         }
 
         .align {
+            flex: 1;
             display: flex;
             align-items: center;
             justify-content: center;
